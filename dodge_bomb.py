826f56774e0a84f70a4122c9c861372e9dfb96d2
Hirotada Fujita <c0b2212931@edu.teu.ac.jp>
2023-10-03 15:22:28 +0900

    練習5

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -5,15 +5,37 @@ import pygame as pg
 
 WIDTH, HEIGHT = 1600, 900
 
-
+delta = {  # 練習３：移動量辞書
+    pg.K_UP: (0, -5),
+    pg.K_DOWN: (0, +5),
+    pg.K_LEFT: (-5, 0),
+    pg.K_RIGHT: (+5, 0),
+}
+ 
+def check_bound(obj_rct: pg.Rect):
+    """
+    引数: こうかとんRectかばくだんRect
+    戻り値: タプル （横方向判定結果、縦方向判定結果）
+    画面内ならTrue、画面外ならFalse
+    """
+    yoko, tate = True, True
+    if obj_rct.left < 0 or WIDTH < obj_rct.right: #横方向判定
+        yoko = False
+    if obj_rct.top < 0 or HEIGHT < obj_rct.bottom: #縦方向判定
+        tate = False
+    return yoko, tate
 
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
+    """工科トン"""
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    kk_rct = kk_img.get_rect()
+    kk_rct.center = (900, 400)
+    """爆弾"""
     clock = pg.time.Clock() #練習1:爆弾Surface作成
     bd_img = pg.Surface((20, 20))
     bd_img.set_colorkey((0, 0, 0)) #練習1:黒い部分を透明にする
@@ -23,16 +45,38 @@ def main():
     bd_rct.center = (x, y)  #練習1:Rectにランダムな座標を設定する
     vx , vy = +5 , +5
 
-
+    
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
+            
+        if kk_rct.colliderect(bd_rct):
+            print("GAMEOVER")
+            return
 
         screen.blit(bg_img, [0, 0])
-        screen.blit(kk_img, [900, 400])
+        
+        """こうかとん"""
+        key_lst = pg.key.get_pressed()
+        sum_mv = [0, 0]
+        for key, mv in delta.items():
+            if key_lst[key]:
+                sum_mv[0] += mv[0]
+                sum_mv[1] += mv[1]
+        kk_rct.move_ip(sum_mv[0], sum_mv[1])
+        if check_bound(kk_rct) != (True, True):
+            kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
+        screen.blit(kk_img, kk_rct) #練習3
+
+        """ばくだん"""
         bd_rct.move_ip(vx, vy) #練習2:爆弾を動かす
+        yoko, tate = check_bound(bd_rct)
+        if not yoko:
+            vx *= -1
+        if not tate:
+            vy *= -1
         screen.blit(bd_img, bd_rct)  #練習1:Rectにランダムな座標に作成
         
         pg.display.update()
